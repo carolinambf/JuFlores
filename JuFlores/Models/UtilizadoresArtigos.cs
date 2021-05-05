9924e5004e72dc2fcd2c9deb30b99d46cd67bd2a
João Almeida <aluno20467@ipt.pt>
2021-05-06 00:25:13 +0100

    Criação de classe que representa a BD

---
+++ JuFlores/Models/UtilizadoresArtigos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JuFlores.Models
+{
+    /// <summary>
+    /// Relaciona os objetos entre as classes Utilizadores e objetos das classes Artigos
+    /// </summary>
+    public class UtilizadoresArtigos
+    {
+        ///fk para os utilziadores 
+        [ForeignKey(nameof(Utilizador))]
+        public int UtilizadorFK { get; set; }
+        public Utilizadores Utilizador { get; set; }
+
+
+
+
+
+
+        ///fk para os artigos 
+        [ForeignKey(nameof(Artigo))]
+        public int ArtigoFK { get; set; }
+        public Utilizadores Artigo { get; set; }
+    }
+}
