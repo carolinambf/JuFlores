98439201bad45cbf1cb2b46b9ffe1d8c9b3edcba
Carolina Ferreira <aluno21071@ipt.pt>
2021-06-25 00:59:47 +0100

    ideia de bot

--- JuFlores/Controllers/HomeController.cs
+++ JuFlores/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ namespace JuFlores.Controllers
         {
             return View();
         }
+
+        public IActionResult Assistente()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
